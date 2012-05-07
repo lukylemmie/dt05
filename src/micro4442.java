f70d5e9723880c26e3eb9bf42ab7ca89eae13801
Andrew Lem <lemmie6@gmail.com>
2012-05-07 12:49:36 +1000

    First commit

---
+++ src/micro4442.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew
+ * Date: 7/05/12
+ * Time: 9:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class micro4442 {
+    public final static int MEM_COUNT = 16;
+
+    int r0;
+    int r1;
+    int[] memAddress = new int[MEM_COUNT];
+
+    public micro4442(){
+
+    }
+
+
+}
